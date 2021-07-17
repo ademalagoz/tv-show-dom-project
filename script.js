d1e629bde119c390d03219c08f06693915ce5b2e
unknown <ademalagoz46@gmail.com>
2021-07-17 18:03:22 +0100

    Level 350

--- script.js
+++ script.js
@@ -1,119 +1,114 @@
 //You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-  createSelect();
+// const allEpisodes = getAllEpisodes();
+
+//Level 350 get data from the Api instead of getAllEpisodes
+let url = "https://api.tvmaze.com/shows/82/episodes";
+function getFetchData(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      makePageForEpisodes(data);
+      selectMenu(data);
+    })
+    .catch((err) => console.log(err));
 }
+// window.onload = setup;
+getFetchData(url);
+
+// function setup() {
+//   makePageForEpisodes(allEpisodes);
+//   // searchEpisodes(allEpisodes);
+//   selectMenu(allEpisodes);
+// }
 
 //LEVEL 100- Get Episodes from getAllEpisodes Function
 
-function makePageForEpisodes(allEpisodes) {
+function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.classList.add("container");
 
   const rowDiv = document.createElement("div");
   rowDiv.classList.add("row");
   const rowMain = document.getElementsByClassName("row");
+
   if (rowMain.length > 0) {
     rowMain[0].remove();
   }
 
-  for (const episode of allEpisodes) {
-    const columnDiv = getTvSeries(episode);
-
+  for (const episode of episodeList) {
+    const columnDiv = oneEpisode(episode);
     rowDiv.appendChild(columnDiv);
   }
 
   rootElem.appendChild(rowDiv);
-} //makePageForEpisodes closing
 
-function getTvSeries(episode) {
-  //start loop
-  const columnDiv = document.createElement("div");
-  columnDiv.classList.add("col-md-4");
+  function oneEpisode(episode) {
+    const columnDiv = document.createElement("div");
+    columnDiv.classList.add("col-md-4");
 
-  const title = document.createElement("h4");
+    const title = document.createElement("h4");
 
-  if (episode.number > 9) {
-    title.innerText = `${episode.name} - S0${episode.season}E${episode.number}`;
-  } else {
-    title.innerText = `${episode.name} - S0${episode.season}E0${episode.number}`;
+    if (episode.number > 9) {
+      title.innerText = `${episode.name} - S0${episode.season}E${episode.number}`;
+    } else {
+      title.innerText = `${episode.name} - S0${episode.season}E0${episode.number}`;
+    }
+
+    const imgElem = document.createElement("img");
+    imgElem.src = episode.image.medium;
+
+    const summaryText = document.createElement("p");
+    summaryText.innerHTML = episode.summary;
+
+    columnDiv.append(title, imgElem, summaryText);
+
+    return columnDiv;
   }
 
-  const imgElem = document.createElement("img");
-  imgElem.src = episode.image.medium;
+  //Level 200-Search Term
 
-  const summaryText = document.createElement("p");
-  summaryText.innerHTML = episode.summary
-    .replace("<p>", "")
-    .replace("</p>", "");
-
-  //Appends
-  columnDiv.append(title, imgElem, summaryText);
-  // columnDiv.appendChild(eachEpisode);
-  //end Loop
-  return columnDiv;
-}
-
-window.onload = setup;
-
-//Level 200-Search and Display Episodes
-
-const searchInput = document.querySelector(".searchInput");
-searchInput.addEventListener("input", makeSearchEpisodes);
-//display
-
-// const navigationDiv = document.querySelector(".navigation");
-// const disPlay = document.querySelector(" disPlayArea");
-
-// disPlay.textContent = `Displaying ${allEpisodes.length}/${allEpisodes.length} episodes`;
-// navigationDiv.appendChild("disPlay");
-
-function makeSearchEpisodes() {
-  const allEpisodes = getAllEpisodes();
+  const countEpisodes = document.querySelector(".displayResults");
+  countEpisodes.innerText = `Display ${episodeList.length} of ${episodeList.length} episodes`;
 
   const searchInput = document.querySelector(".searchInput");
-  const searchValue = searchInput.value.toUpperCase();
+  searchInput.addEventListener("input", searchEpisodes);
 
-  const filteredEpisodes = allEpisodes.filter((episode) => {
-    const upperName = episode.name.toUpperCase();
-    const upperSummary = episode.summary.toUpperCase();
+  function searchEpisodes() {
+    let value = searchInput.value.toLowerCase();
+    const allColumns = document.querySelectorAll(".col-md-4");
+    let count = 0;
+    countEpisodes.innerText = "";
 
-    if (upperName.includes(searchValue) || upperSummary.includes(searchValue)) {
-      return episode;
-    }
-  }); //filter close
-
-  // console.log(filteredEpisodes);
-
-  const displayResults = document.querySelector(".displayResults");
-
-  if (searchValue === "") {
-    displayResults.innerText = `Displaying ${allEpisodes.length}/${allEpisodes.length} episodes`;
-  } else {
-    displayResults.innerText = `Displaying ${filteredEpisodes.length}/${allEpisodes.length} episodes`;
+    allColumns.forEach((column) => {
+      const columnValue = column.textContent.toLowerCase();
+      if (columnValue.includes(value)) {
+        column.style.display = "";
+        count++;
+        countEpisodes.innerText = `Display ${count} of ${allColumns.length} episodes`;
+      } else {
+        column.style.display = "none";
+        countEpisodes.innerText = `Display ${count} of ${allColumns.length} episodes`;
+      }
+    });
   }
+} //makePageForEpisodes closing
 
-  makePageForEpisodes(filteredEpisodes);
-} //function close
+//Level 300-Select/Option Menu
 
-//Level 300-Select Episode
-
-function createSelect() {
-  //create content of select
+function selectMenu(wholeEpisodes) {
   const selectBtn = document.querySelector(".selectButton");
-  const allEpisodes = getAllEpisodes();
 
-  //Option Main
   const optionEpisode = document.createElement("option");
   optionEpisode.value = "-1";
   optionEpisode.innerHTML = "Select an Episode";
+
   selectBtn.appendChild(optionEpisode);
-  //Other Options
-  for (let i = 0; i < allEpisodes.length; i++) {
-    const episode = allEpisodes[i];
+
+  for (let i = 0; i < wholeEpisodes.length; i++) {
+    const episode = wholeEpisodes[i];
     const option = document.createElement("option");
-    option.value = `${i}`; // show.id
+    option.value = `${i}`;
 
     if (episode.number > 9) {
       option.innerText = `${episode.name} - S0${episode.season}E${episode.number}`;
@@ -124,29 +119,12 @@ function createSelect() {
     selectBtn.appendChild(option);
   }
 
-  selectBtn.addEventListener("change", selectFunc);
-}
+  selectBtn.addEventListener("change", searchValue);
 
-function selectFunc(e) {
-  const index = parseInt(e.target.value);
-  //  console.log(index);
-  const allEpisodes = getAllEpisodes();
-
-  if (index === -1) {
-    console.log(allEpisodes[index]);
-    makePageForEpisodes(allEpisodes);
-  } else {
-    console.log(allEpisodes[index]);
-    makePageForEpisodes([allEpisodes[index]]);
+  function searchValue(e) {
+    const index = parseInt(e.target.value);
+    index === -1
+      ? makePageForEpisodes(wholeEpisodes)
+      : makePageForEpisodes([wholeEpisodes[index]]);
   }
-
-  //event value
 }
-
-//LEVEL 300
-// Complete all requirements from level 200
-// Add a select input which allows you to jump quickly to an episode:
-// The select input should list all episodes in the format: "S01E01 - Winter is Coming"
-// When the user makes a selection, they should be taken directly to that episode in the list
-// Bonus: if you prefer, when the select is used, ONLY show the selected episode. If you do this,
-//  be sure to provide a way for the user to see all episodes again.
